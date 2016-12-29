a107bec7880c33d556d409821a5251c5207b3c8c
Rico Cofani <kontakt@ricofani.de>
2016-12-29 11:16:18 +0100

    Completed sticky header functionality

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,38 +1,33 @@
 var gulp = require( 'gulp' );
 var watch = require( 'gulp-watch' );
-var browserSync = require( 'browser-sync' )
-.create();
+var browserSync = require( 'browser-sync' ).create();
 
 
-gulp.task( 'watch', function()
-{
+gulp.task( 'watch', function() {
     browserSync.init( {
-        notify: false, // don't show message box when refreshing
+        notify : false, // don't show message box when refreshing
         server : {
             baseDir : 'app'
         }
     } );
-    watch( './app/index.html', function()
-    {
+    watch( './app/index.html', function() {
         browserSync.reload();
     } );
-    watch( './app/assets/styles/**/*.css', function()
-    {
+    watch( './app/assets/styles/**/*.css', function() {
         gulp.start( 'cssInject' );
     } );
-    watch( './app/assets/scripts/**/*.js', function()
-    {
+    watch( './app/assets/scripts/**/*.js', function() {
         gulp.start( 'scriptsRefresh' );
     } );
 } );
 
-gulp.task( 'cssInject', ['styles'], function() // first run and complete styles-task before cssInject
+gulp.task( 'cssInject', [ 'styles' ], function() // first run and complete styles-task before cssInject
 {
     return gulp.src( './app/temp/styles/styles.css' )
     .pipe( browserSync.stream() );
 } );
 
-gulp.task( 'scriptsRefresh', ['scripts'], function() // first run and complete scriptsRefresh-task before scriptsRefresh
+gulp.task( 'scriptsRefresh', [ 'scripts' ], function() // first run and complete scriptsRefresh-task before scriptsRefresh
 {
     browserSync.reload();
 } );
