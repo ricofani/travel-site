3908997bc427095f75c2ad3081d82e34e9418a9c
Rico Cofani <kontakt@ricofani.de>
2016-12-29 17:05:39 +0100

    Added browser support fallbacks.

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,12 +1,9 @@
 var gulp = require( 'gulp' );
 var webpack = require( 'webpack' );
 
-gulp.task( 'scripts', function( callback )
-{
-    webpack( require( '../../webpack.config.js' ), function( err, stats )
-    {
-        if( err )
-        {
+gulp.task( 'scripts', [ 'modernizr' ], function( callback ) {
+    webpack( require( '../../webpack.config.js' ), function( err, stats ) {
+        if( err ) {
             console.log( err.toString() );
         }
         console.log( stats.toString() );
--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,49 +1,76 @@
-var gulp = require('gulp');
-var svgSprite = require('gulp-svg-sprite');
-var rename = require('gulp-rename');
-var del = require('del');
+var gulp = require( 'gulp' );
+var svgSprite = require( 'gulp-svg-sprite' );
+var rename = require( 'gulp-rename' );
+var del = require( 'del' );
+var svg2png = require( 'gulp-svg2png' );
 
 var svgSpriteConfig = {
-    mode:{
-        css: {
-            sprite: 'sprite.svg',
-            render: {
-                css: {
-                    template: './gulp/templates/sprite.css'
+    shape : {
+        spacing : {
+            padding : 1
+        }
+    },
+    mode  : {
+        css : {
+            variables : {
+                replaceSvgWithPng : function() {
+                    return function( sprite, render ) {
+                        return render( sprite ).split( '.svg' ).join( '.png' );
+                    }
+                }
+            },
+            sprite    : 'sprite.svg',
+            render    : {
+                css : {
+                    template : './gulp/templates/sprite.css'
                 }
             }
         }
     }
 };
 
-gulp.task('beginClean', function()
-{
-    return del(['./app/temp/sprite', './app/assets/images/sprites']);
-});
+gulp.task( 'beginClean', function() {
+    return del( [
+        './app/temp/sprite',
+        './app/assets/images/sprites'
+    ] );
+} );
 
-gulp.task('createSprite', ['beginClean'], function()
-{
-    return gulp.src('./app/assets/images/icons/**/*.svg')
-    .pipe(svgSprite(svgSpriteConfig))
-    .pipe(gulp.dest('./app/temp/sprite/'));
-});
+gulp.task( 'createSprite', [ 'beginClean' ], function() {
+    return gulp.src( './app/assets/images/icons/**/*.svg' )
+    .pipe( svgSprite( svgSpriteConfig ) )
+    .pipe( gulp.dest( './app/temp/sprite/' ) );
+} );
 
-gulp.task('copySpriteGraphic', ['createSprite'], function()
-{
-    return gulp.src('./app/temp/sprite/css/**/*.svg')
-    .pipe(gulp.dest('./app/assets/images/sprites'));
-});
+gulp.task( 'createPngCopy', [ 'createSprite' ], function() {
+    return gulp.src( './app/temp/sprite/css/*.svg' )
+    .pipe( svg2png() )
+    .pipe( gulp.dest( './app/temp/sprite/css' ) );
+} );
 
-gulp.task('copySpriteCSS', ['createSprite'], function()
-{
-    return gulp.src('./app/temp/sprite/css/*.css')
-    .pipe(rename('_sprite.css'))
-    .pipe(gulp.dest('./app/assets/styles/modules/'));
-});
+gulp.task( 'copySpriteGraphic', [ 'createPngCopy' ], function() {
+    return gulp.src( './app/temp/sprite/css/**/*.{svg,png}' )
+    .pipe( gulp.dest( './app/assets/images/sprites' ) );
+} );
 
-gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function()
-{
-    return del(['./app/temp/sprite']);
-});
+gulp.task( 'copySpriteCSS', [ 'createSprite' ], function() {
+    return gulp.src( './app/temp/sprite/css/*.css' )
+    .pipe( rename( '_sprite.css' ) )
+    .pipe( gulp.dest( './app/assets/styles/modules/' ) );
+} );
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task( 'endClean', [
+    'copySpriteGraphic',
+    'copySpriteCSS'
+], function() {
+    return del( [ './app/temp/sprite' ] );
+} );
+
+gulp.task( 'icons', [
+    'beginClean',
+    'createSprite',
+    'createPngCopy',
+    'copySpriteGraphic',
+    'copySpriteCSS',
+    'endClean'
+] );
